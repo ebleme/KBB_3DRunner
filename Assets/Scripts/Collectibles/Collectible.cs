d20b39feafdd39140b6a32beb93fc99a3b5b12fa
Mehmet Ali <maebleme@gmail.com>
2024-12-22 16:36:00 +0300

    Removed comment lines

--- Assets/Scripts/Collectibles/Collectible.cs
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -1,4 +1,3 @@
-using System;
 using DG.Tweening;
 using Ebleme.KBB3DRunner.Ant;
 using UnityEngine;
@@ -14,22 +13,9 @@ public class Collectible : MonoBehaviour
     [SerializeField] private float energyIncrease = 10;
 
 
-    /*
-     * + Karınca temas ettiği an
-     * + Yok olacak
-     * + Particle fx
-     * + Ses fx
-     * + Karıncanın enerjisi artacak (EnergyBar yönetecek) Ne kadar?
-     * + Idle - Etrafında dönme animasyonu
-     * + Yukarı aşağı hareket
-     */
-
     private Sequence seq;
     private void Start()
     {
-        // rotateTween = transform.DORotate(new Vector3(0, 360, 0), rotateDuration, RotateMode.FastBeyond360)
-        //     .SetEase(Ease.Linear).SetLoops(-1);
-
         seq = DOTween.Sequence();
         seq.Append(transform.DOMoveY(1, bounceDuration));
         seq.Insert(0,
@@ -38,24 +24,11 @@ public class Collectible : MonoBehaviour
         seq.Append(transform.DOMoveY(0, bounceDuration));
         seq.Insert(1, transform.DORotate(new Vector3(0, -360, 0), rotateDuration, RotateMode.FastBeyond360));
         seq.SetLoops(-1);
-
-        // transform.DOPunchPosition(Vector3.up, bounceDuration, 0).SetLoops(-1);
-
     }
     
-
-    // private void Update()
-    // {
-    //     transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
-    //     
-    // }
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Karınca temas etti");
         Destroy(gameObject);
-
-        // FindAnyObjectByType<Player>().Collected(energyIncrease);
 
         Player.Instance.Collected(energyIncrease);
     }
